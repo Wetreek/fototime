6ffc134784ae3344b530841e2c2a119e4abf1a71
Izabela Trepáčová <izabela.trepacova@gmail.com>
2019-10-15 16:28:35 +0200

    pokus o lepsiu photogallery

--- resources/views/photoGallery.blade.php
+++ resources/views/photoGallery.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
 <div class="row gallery justify-content-center"> 
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
       <img src="https://via.placeholder.com/600x400">
     </div>
-    <div class="col-md-4 gallery-item">
-        <img src="https://via.placeholder.com/600x400">        
+    <div class="gallery-item column">
+        <img src="https://via.placeholder.com/400x600">        
     </div>
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
             <img src="https://via.placeholder.com/600x400">        
         </div>
-    <div class="col-md-4 gallery-item">
-            <img src="https://via.placeholder.com/600x400">        
-        </div>
-    <div class="col-md-4 gallery-item">
-            <img src="https://via.placeholder.com/600x400">        
-        </div>
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
             <img src="https://via.placeholder.com/400x600">        
         </div>
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
             <img src="https://via.placeholder.com/600x400">        
         </div>
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
             <img src="https://via.placeholder.com/400x600">        
         </div>
-    <div class="col-md-4 gallery-item">
+    <div class="gallery-item column">
+            <img src="https://via.placeholder.com/600x400">        
+        </div>
+    <div class="gallery-item column">
+            <img src="https://via.placeholder.com/400x600">        
+        </div>
+    <div class="gallery-item column">
             <img src="https://via.placeholder.com/600x400">        
         </div>
   </div>
