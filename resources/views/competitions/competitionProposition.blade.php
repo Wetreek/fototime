7410ce768eda67dd77dc3e98279a129cb51a46a5
Izabela Trepáčová <izabela.trepacova@gmail.com>
2019-11-12 20:27:07 +0100

    error competitionProposition

--- resources/views/competitions/competitionProposition.blade.php
+++ resources/views/competitions/competitionProposition.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 {{--@include( 'competitions.competition')--}}
 @foreach ($proposition as $item)
-    @if ($item->proposition_type_id==2)
+    @if ($item->proposition_type_id==config('app.COMPETITION_PROPOSITIONS_MEDIAL_PARTNERS'))
         <div class="text">POROTA:   {!!$item->proposition_text!!}</div>
     @elseif ($item->proposition_type_id==4)
         <div class="text">POROTA:   {!!$item->proposition_text!!}</div>
